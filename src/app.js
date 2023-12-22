826acdee2d2d2e3a0c24462ef2cacf748918d7a6
Rodrigo Herrainz <rherrainz@gmail.com>
2023-12-21 22:14:11 -0300

    added services/db/controllers and routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import express from "express";
+import { config } from "./config.js";
+import db from "./db/dbConfig.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = config.port;
+
+const dbConnect = async () => {
+  try {
+    await db;
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+dbConnect();
+
+app.get("/", (req, res) => {
+  res.send(`<h1 style="font-family: helvetica, arial">Hello World!</h1>`);
+});
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
