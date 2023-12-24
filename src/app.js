755d9cd1069d11bd37afd99624a3ad34edc285fb
Rodrigo Herrainz <rherrainz@gmail.com>
2023-12-23 23:26:41 -0300

    added index router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "./config.js";
 import db from "./db/dbConfig.js";
+import  router  from "./routes/indexRoute.js";
 
 const app = express();
 const PORT = config.port;
@@ -15,6 +16,11 @@ const dbConnect = async () => {
 };
 
 dbConnect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/", router);
 
 app.get("/", (req, res) => {
   res.send(`<h1 style="font-family: helvetica, arial">Hello World!</h1>`);
